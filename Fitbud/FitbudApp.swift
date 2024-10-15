41988794ab08fe1b844dd183894dfcfc337d379a
laitoku <9041246+laitoku@users.noreply.github.com>
2024-10-15 16:49:44 -0700

    Initial Commit

---
+++ Fitbud/FitbudApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitbudApp.swift
+//  Fitbud
+//
+//  Created by Joshua Lai on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FitbudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
